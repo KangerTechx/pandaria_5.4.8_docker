0c58083bf6baab3d16817fe830e7f88d5397f06f
SEB\sbbla <sbblache@gmail.com>
2026-01-18 14:59:21 +0100

    change path for client install to try to extract data

--- docker/utility/commands/install_client.sh
+++ docker/utility/commands/install_client.sh
@@ -11,7 +11,7 @@ GITHUB_RELEASE_TAG="client-mop-5.4.8"
 
 BASE_DIR="$(cd "$(dirname "$0")/../../.." && pwd)"
 DOWNLOADS_DIR="$BASE_DIR/app/downloads"
-CLIENT_DIR="$BASE_DIR/app/client"
+CLIENT_DIR="${WOW_INTERNAL:-/app/wow}"
 
 mkdir -p "$DOWNLOADS_DIR" "$CLIENT_DIR"
 
